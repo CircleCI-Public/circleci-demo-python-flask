2cf45cb476e3f6a85ae73d01ddaff8fc836806b8
Alek Sharma <aleksharma12@gmail.com>
2018-05-07 16:48:12 -0700

    Revert "Add workflows and update heroku config"

---
+++ .circleci/setup-heroku.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+git remote add heroku https://git.heroku.com/circleci-demo-python-flask.git
+wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
+sudo mkdir -p /usr/local/lib /usr/local/bin
+sudo tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
+sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
+
+cat > ~/.netrc << EOF
+machine api.heroku.com
+  login $HEROKU_LOGIN
+  password $HEROKU_API_KEY
+machine git.heroku.com
+  login $HEROKU_LOGIN
+  password $HEROKU_API_KEY
+EOF
